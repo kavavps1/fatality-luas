c1482658c8e35dc738518cd40ef521978750d87c
kavavps1 <47143116+kavavps1@users.noreply.github.com>
2025-01-04 09:11:30 +0100

    release
    dogshit AA with knife in your hand, don't use it for your sanity

---
+++ ufo_hide_head.lua
@@ -0,0 +1,24 @@
+--Hide Head
+--By Kava LUA GOD dc: ufohook
+local hide_head_cb = gui.checkbox(gui.control_id('hide_head'))
+local row = gui.make_control('Hide head', hide_head_cb)
+local group = gui.ctx:find('lua>elements a')
+group:add(row);
+
+local function on_present_queue()
+    if hide_head_cb:get_value():get() then
+        local local_player = entities.get_local_pawn()
+        local on_ground = bit.band(local_player.m_fFlags:get(), bit.lshift(1, 0))
+        --local velocity_modifier = local_player.m_flVelocityModifier:get()
+        --print(on_ground)
+        local wep = local_player:get_active_weapon();
+        if wep == null then return end
+        local type = wep:get_type();
+        if type ~= 0 then return end
+        if on_ground == 0 then
+            game.engine:client_cmd("switchhands")
+        end
+    end      
+end
+
+events.present_queue:add(on_present_queue);
